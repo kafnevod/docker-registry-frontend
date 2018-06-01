9a89a9638c67fcdac5bcf7b597e670c76d1059b2
Alexey Kostarev <kaf@nevod.ru>
2018-06-01 16:36:26 +0500

    Additional scripts + rename imag name

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+docker build \
+        -t dh.ics.perm.ru/konradkleine/docker-registry-frontend \
+        .
+
---
+++ exec.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#!/bin/sh
+docker exec -it registryInterface bash
